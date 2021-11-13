e88675701fc7774a0b638e307c6a265b2bb540c1
htdream <hoang88thai@gmail.com>
2021-11-12 22:57:11 -0500

    changed Upper P to lower p

--- BatchFile/AllLibrary.bat
+++ BatchFile/AllLibrary.bat
@@ -1,5 +1,5 @@
-Pip install requests
-Pip install pytest
-Pip install jsonpath
-Pip install openpyxl
+pip install requests
+pip install pytest
+pip install jsonpath
+pip install openpyxl
 pip install allure-pytest
